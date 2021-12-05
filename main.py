f2171f75270503e2455a266d5bf3fcf4cd0d50c2
ruuffian <ruuffian@gmail.com>
2021-12-04 23:55:33 -0500

    many bugs.

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ import urllib.parse
 print("Welcome to ruuffian's tex-converter! Hopefully,"
       " this will allow you to host your obsidian notes "
       "on github and not lose the power that latex brings!")
+param = "^\\$.*\\$$"
+hack1 = "<img src=\"https://render.githubusercontent.com/render/math?math="
+hack2 = "\">"
 
-
-# plus signs need to be replaced by %2B
 
 # Step 1: find latex in file
 # Step 2: encode formula
 # Step 3: plug it into github markdown img link
 # Step 4: replace in filestring, then write to a new file
+
 
 def readfile(filename):
     f = open(filename, "r")
@@ -20,15 +22,22 @@ def readfile(filename):
     return fstr
 
 
-param = "^\\$.*\\$$"
-hack1 = "<img src=\"https://render.githubusercontent.com/render/math?math="
-hack2 = "\">"
+# need to strip $ symbols before encoding
 
 
 def applyregex(filestring, regex):
     match = re.search(regex, filestring)
     while match != "None":
-        re.sub(match.string, hack1 + urllib.parse.quote(match.group()) + hack2, filestring)
+        re.sub(match.string, hack1 + urllib.parse.quote(match.group().strip("$")) + hack2, filestring)
+    return filestring
 
-# need to implement new file write
 
+def writefile(filestring):
+    f = open("git.txt", "w")
+    f.write(filestring)
+    f.close()
+
+
+string = readfile("test.txt")
+converted = applyregex(string, param)
+writefile(converted)
